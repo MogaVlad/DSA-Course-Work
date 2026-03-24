f58350d228c6ca156f8bc4eed244704aab6ba5af
MogaVlad <vladmmoga@gmail.com>
2026-03-24 18:27:46 +0200

    Initial commit

---
+++ Assignment2_LinkedList/src/App.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../include/PriorityQueue.h"
+#include "../include/ExtendedTest.h"
+#include "../include/ShortTest.h"
+
+using namespace std;
+
+
+
+
+int main() {
+	testAll();
+	testAllExtended();
+
+	cout << "End" << endl;
+	system("pause");
+
+}
